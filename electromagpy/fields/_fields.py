bfbffaa663210d18519999d1d8b416f1b5730b5b
dmerthe <36462234+dmerthe@users.noreply.github.com>
2025-02-01 02:44:42 -0800

    Update _fields.py

--- electromagpy/fields/_fields.py
+++ electromagpy/fields/_fields.py
@@ -1,26 +1,10 @@
 # Electromagnetic fields, either explicitly defined or generated from bodies
-import cython
+
+from numba import jit, float64, void
 import numpy as np
-import cython.cimports.numpy as np
 
 
-@cython.cfunc
-def dot(v1: cython.double[3], v2: cython.double[3]) -> cython.double:
-    """Compute the dot product of two 3-vectors"""
-    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
-
-@cython.cfunc
-def cross(i: cython.int, v1: cython.double[3], v2: cython.double[3]) -> cython.double:
-    """Compute the ith element of the cross product of two 3-vectors"""
-
-    j: cython.int = (i + 1) % 3
-    k: cython.int = (i + 2) % 3
-
-    return v1[j] * v2[k] - v1[k] * v2[j]
-
-
-@cython.cclass
-class _Field:
+class Field:
     """
     Base class for all electromagnetic fields.
 
@@ -28,20 +12,13 @@ class _Field:
     potential (A) and magnetic field (B).
     """
 
-    _V: cython.double  # electric potential
-    _E: cython.double[3]  # electric field
-    _A: cython.double[3]  # magnetic potential
-    _B: cython.double[3]  # magnetic field
+    _V = 0.0  # electric potential
+    _E = np.array([0.0, 0.0, 0.0])  # electric field
+    _A = np.array([0.0, 0.0, 0.0])  # magnetic potential
+    _B = np.array([0.0, 0.0, 0.0])  # magnetic field
 
-    def __cinit__(self):
-
-        self._V = 0.0
-        self._E[0] = self._E[1] = self._E[2] = 0.0
-        self._A[0] = self._A[1] = self._A[2] = 0.0
-        self._B[0] = self._B[1] = self._B[2] = 0.0
-
-    @cython.cfunc
-    def _eval_V(self, r: cython.double[3], t: cython.double) -> cython.void:
+    @jit(void(float64[:], float64))
+    def _eval_V(self, r, t):
         """Evaluate the electric potential at the point r and time t"""
         pass
 
@@ -93,31 +70,3 @@ class _Field:
         B_view = cython.declare(cython.double[:], self._B)
 
         return B_view
-
-
-def py_field_factory(cfield):
-    """
-    Generate the python wrapper for the given C field class
-    """
-
-    class PythonField:
-
-        def __init__(self, *args):
-            self._field = cfield(*args)
-
-        def V(self, r, t):
-            return self._field.V(r, t)
-
-        def E(self, r, t):
-            return np.array([Ei for Ei in self._field.E(r, t)])
-
-        def A(self, r, t):
-            return np.array([Ai for Ai in self._field.A(r, t)])
-
-        def B(self, r, t):
-            return np.array([Bi for Bi in self._field.B(r, t)])
-
-    return PythonField
-
-
-Field = py_field_factory(_Field)
